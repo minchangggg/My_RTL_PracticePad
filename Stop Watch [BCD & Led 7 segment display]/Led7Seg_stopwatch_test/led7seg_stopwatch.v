814839721925fe4fda4c3fe36f5ebd03a33c3fe3
meynchan <125820144+minchangggg@users.noreply.github.com>
2025-09-07 01:26:33 +0700

    Update led7seg_stopwatch.v

--- Stop Watch [BCD & Led 7 segment display]/Led7Seg_stopwatch_test/led7seg_stopwatch.v
+++ Stop Watch [BCD & Led 7 segment display]/Led7Seg_stopwatch_test/led7seg_stopwatch.v
@@ -142,30 +142,30 @@ module bcd_stopwatch (
   endfunction
 endmodule
 
-// 7-seg decoder (common cathode)
+// 7-seg decoder (common anode)
 module seg7_decoder(
   input wire [3:0] bcd, 
   output reg [6:0] seg
 );
   always @(*) begin
     case (bcd)
-      4'd0: seg=7'b1000000; 
-      4'd1: seg=7'b1111001; 
-      4'd2: seg=7'b0100100;
-      4'd3: seg=7'b0110000; 
-      4'd4: seg=7'b0011001; 
-      4'd5: seg=7'b0010010;
-      4'd6: seg=7'b0000010; 
-      4'd7: seg=7'b1111000; 
-      4'd8: seg=7'b0000000;
-      4'd9: seg=7'b0010000; 
+      4'd0: seg = 7'b0000001; 
+      4'd1: seg = 7'b1001111;
+      4'd2: seg = 7'b0010010; 
+      4'd3: seg = 7'b0000110;
+      4'd4: seg = 7'b1001100;
+      4'd5: seg = 7'b0100100;
+      4'd6: seg = 7'b0100000;
+      4'd7: seg = 7'b0001111;
+      4'd8: seg = 7'b0000000;
+      4'd9: seg = 7'b0000100;
       default: seg=7'b1111111;
     endcase
   end
 endmodule
 
 /*
-// 7-seg decoder (common anode)
+// 7-seg decoder (common cathode)
 module seg7_decoder(
   input wire [3:0] bcd, 
   output reg [6:0] seg
