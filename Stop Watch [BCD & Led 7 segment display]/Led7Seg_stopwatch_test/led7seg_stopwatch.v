63d8d5e6ed59be1ca67e5c1c6534384e0d147e98
meynchan <125820144+minchangggg@users.noreply.github.com>
2025-09-07 01:16:46 +0700

    Update led7seg_stopwatch.v

--- Stop Watch [BCD & Led 7 segment display]/Led7Seg_stopwatch_test/led7seg_stopwatch.v
+++ Stop Watch [BCD & Led 7 segment display]/Led7Seg_stopwatch_test/led7seg_stopwatch.v
@@ -143,7 +143,10 @@ module bcd_stopwatch (
 endmodule
 
 // 7-seg decoder (common cathode)
-module seg7_decoder(input wire [3:0] bcd, output reg [6:0] seg);
+module seg7_decoder(
+  input wire [3:0] bcd, 
+  output reg [6:0] seg
+);
   always @(*) begin
     case (bcd)
       4'd0: seg=7'b1000000; 
@@ -160,3 +163,27 @@ module seg7_decoder(input wire [3:0] bcd, output reg [6:0] seg);
     endcase
   end
 endmodule
+
+/*
+// 7-seg decoder (common anode)
+module seg7_decoder(
+  input wire [3:0] bcd, 
+  output reg [6:0] seg
+);
+  always @(*) begin
+    case (bcd)
+      4'd0: seg=7'b1111110; 
+      4'd1: seg=7'b0110000; 
+      4'd2: seg=7'b1101101;
+      4'd3: seg=7'b1111001; 
+      4'd4: seg=7'b0110011; 
+      4'd5: seg=7'b1011011;
+      4'd6: seg=7'b1011111; 
+      4'd7: seg=7'b1110000; 
+      4'd8: seg=7'b1111111;
+      4'd9: seg=7'b1111011; 
+      default: seg=7'b000000;
+    endcase
+  end
+endmodule
+*/
